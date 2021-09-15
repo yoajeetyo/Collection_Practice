5c7b113fea0f7171d63bc1691cb992c8fa76707f
yoajeetyo <sajeetkumar40@gmail.com>
2021-09-15 19:01:29 +0530

    Added : ArrayList iterate using for each loop

---
+++ src/com/bl/collection/ArrayListIterateUsingForEach.java
@@ -0,0 +1,22 @@
+package com.bl.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListIterateUsingForEach {
+
+	 public static void main(String args[]) {
+	        // initialize ArrayList  
+	        ArrayList<Integer> al = new ArrayList<Integer>();
+	        // add elements to ArrayList object
+	        al.add(3);
+	        al.add(17);
+	        al.add(6);
+	        al.add(9);
+	        al.add(7);
+	        System.out.println("Using Advanced For Loop");   
+	        // printing ArrayList 
+	        for (Integer num : al) {         
+	            System.out.println(num);   
+	        }
+	    }
+}
